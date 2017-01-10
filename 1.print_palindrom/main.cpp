e04430f5a49dc1c485d56ac8163a46c276ae1297
PravdiilShilinskas <mr.pravdiil@mail.ru>
2017-01-10 22:56:07 +0300

    Update main.cpp

--- 1.print_palindrom/main.cpp
+++ 1.print_palindrom/main.cpp
@@ -3,11 +3,24 @@ using namespace std;
 
 void print_palindrom(int n){
     int palindrom[n];
+    int summ;
     for(int i=0;i<n/2;i++){
         for(int q=0;q<10;q++){
             palindrom[i]=q;
             palindrom[n-1-i]=q;
             cout << palindrom[i] << palindrom[n-1-i];
+            summ=2*q;
+        }
+    }
+    if((n%2)!=0){
+        for(int i=0;i<10;i++){
+            palindrom[n/2]=i;
+            if((summ+i)%9==0){
+                for(int q=0;q<n;q++){
+                    cout << palindrom[q];
+                }
+                cout << "   ";
+            }
         }
     }
 }
