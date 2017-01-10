79e1f69c4202b6e580a14fcde5bb73f77d0d1de5
PravdiilShilinskas <mr.pravdiil@mail.ru>
2017-01-10 23:09:44 +0300

    Update main.cpp

--- 9.darts_game/main.cpp
+++ 9.darts_game/main.cpp
@@ -1,1 +1,9 @@
-ЫЫЫ
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i=0;i<666;i++) {
+        cout << "Без малейшего понятия, как это делать. =)" << "\n";
+    }
+    return 0;
+}
