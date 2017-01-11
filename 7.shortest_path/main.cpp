690c12a7351a0bc0a3520b96b80e90b2731c3ead
PravdiilShilinskas <mr.pravdiil@mail.ru>
2017-01-11 10:53:38 +0300

    Update main.cpp

--- 7.shortest_path/main.cpp
+++ 7.shortest_path/main.cpp
@@ -1,1 +1,15 @@
-Ыыы
+#include <iostream>
+using namespace std;
+
+int shortest_path(int x, int  y){
+    int cs;
+    cin >> cs;
+    int Scver[cs][cs];
+}
+
+int main(){
+  int x, y;
+  cin >> x >> y;
+  cout << shortest_path(x, y);
+  return 0;
+}
