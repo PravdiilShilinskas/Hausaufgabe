a2a761a07173b2a44a1a587ad5d7534e1b9455f3
PravdiilShilinskas <mr.pravdiil@mail.ru>
2017-01-17 21:11:11 +0300

    Update main.cpp

--- 6.shedule/main.cpp
+++ 6.shedule/main.cpp
@@ -1,1 +1,29 @@
-Ыыы
+#include <iostream>
+using namespace std;
+
+void shedule(){
+    int n, max;
+    cin >> n;
+    int s[n], f[n], udacha[n];
+    for(int i=0;i<n;i++){
+        udacha[i]=1;
+    }
+    for(int i=0;i<n;i++){
+        cin >> s[i] >> f[i];
+    }
+  
+    for(int i=0;i<n;i++){
+        for(int q=0;q<n;q++){
+            if(f[i]<=s[q]) udacha[i]+=1;
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(max<udacha[i]) max=udacha[i];
+    }
+    cout << max;
+}
+
+int main(){
+    shedule();
+    return 0;
+}
