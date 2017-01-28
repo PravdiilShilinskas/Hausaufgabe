875e996a214cb90fde3340a2afaef03fbe157264
PravdiilShilinskas <mr.pravdiil@mail.ru>
2017-01-29 01:31:14 +0300

    Update main.cpp

--- dz_po_classu_string/main.cpp
+++ dz_po_classu_string/main.cpp
@@ -7,32 +7,42 @@ typedef class string{
     public:
     string(int x){
         size=x;
-        Array=new char*[size];
+        Array=new char[size];
         for(int c=0;c<size;c++) cin >> Array[c];
     }
-    concat(string s){
-        a.size+=s.size;
-        char temp[a.size];
-        int tempik;
-        
+    void concat(string S){
+        for(int c=size;c<size+S.size;c++)Array[c]=S.Array[c-size];
+        size+=S.size;
     }
-    find(string s){
-        
+    void find(string S){
+        if(size>=S.size){
+            int recurs(int x, int y){
+                if(Array[x+y]==S.Array[y]){
+                    if(S.size>y+1) recurs(x, y+1);
+                    else return 1;
+                }
+                return 0;
+            }
+            for(int c=0;c<size+1-S.size;c++){
+                int nomer=0;
+                if(recurs(c, nomer)==1) cout << c << endl; 
+            }
+        }
     }
-    print(){
+    void print(){
         for(int c=0;c<size;c++) cout << Array[c];
     }
-    set(int i){
+    void set(int i){
         cin >> Array[i];
     }
-    get(int i){
-        cout << Array[i];
+    void get(int i){
+        cout << Array[i] << endl;
     }
     ~string(){
         for(int c=0;c<size;c++){
             delete Array[c];
-            delete Array;
         }
+        delete Array;
     }
 }string;
 
